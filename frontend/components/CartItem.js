fcdff459c31ac9d68d92803e9181af59146823ff
Student <student@owners-MacBook-Pro.local>
2019-01-12 18:04:36 -0500

    Completed search with Dropdown

--- frontend/components/CartItem.js
+++ frontend/components/CartItem.js
@@ -18,22 +18,32 @@ const CartItemStyles = styled.li`
     margin: 0;
   }
 `;
-const CartItem = ({ cartItem }) => (
-  <CartItemStyles>
-    <img width='80' src={cartItem.item.image} alt={cartItem.item.title} />
-    <div className='cart-item-details'>
-      <h3>{cartItem.item.title}</h3>
-      <p>
-        {formatMoney(cartItem.item.price * cartItem.quantity)}
-        {' - '}
-        <em>
-          {cartItem.quantity} &times; {formatMoney(cartItem.item.price)} each
-        </em>
-      </p>
-    </div>
-    <RemoveFromCart id={cartItem.id} />
-  </CartItemStyles>
-);
+const CartItem = ({ cartItem }) => {
+  //first check if that item exists - it could be deleted from the store
+  if (!cartItem.item)
+    return (
+      <CartItemStyles>
+        <p>This item is no longer available</p>
+        <RemoveFromCart id={cartItem.id} />
+      </CartItemStyles>
+    );
+  return (
+    <CartItemStyles>
+      <img width='80' src={cartItem.item.image} alt={cartItem.item.title} />
+      <div className='cart-item-details'>
+        <h3>{cartItem.item.title}</h3>
+        <p>
+          {formatMoney(cartItem.item.price * cartItem.quantity)}
+          {' - '}
+          <em>
+            {cartItem.quantity} &times; {formatMoney(cartItem.item.price)} each
+          </em>
+        </p>
+      </div>
+      <RemoveFromCart id={cartItem.id} />
+    </CartItemStyles>
+  );
+};
 
 CartItem.propTypes = {
   cartItem: PropTypes.object.isRequired
---
+++ frontend/components/Search.js
@@ -0,0 +1,114 @@
+import React from 'react';
+import Downshift, { resetIdCounter } from 'downshift';
+import Router from 'next/router';
+import { ApolloConsumer } from 'react-apollo';
+import gql from 'graphql-tag';
+import debounce from 'lodash.debounce';
+import { DropDown, DropDownItem, SearchStyles } from './styles/DropDown';
+import { render } from 'react-dom';
+
+const SEARCH_ITEMS_QUERY = gql`
+  query SEARCH_ITEMS_QUERY($searchTerm: String!) {
+    items(
+      where: {
+        OR: [
+          { title_contains: $searchTerm }
+          { description_contains: $searchTerm }
+        ]
+      }
+    ) {
+      id
+      image
+      title
+    }
+  }
+`;
+
+function routeToItem(item) {
+  Router.push({
+    pathname: '/item',
+    query: {
+      id: item.id
+    }
+  });
+}
+
+class AutoComplete extends React.Component {
+  state = {
+    items: [],
+    loading: false
+  };
+
+  //debounce will delay the search in 350 ms.
+  //to avoid querying for every single char typed in
+  onChange = debounce(async (e, client) => {
+    //turn loading on
+    this.setState({ loading: true });
+    const res = await client.query({
+      query: SEARCH_ITEMS_QUERY,
+      variables: { searchTerm: e.target.value }
+    });
+    this.setState({
+      items: res.data.items,
+      loading: false
+    });
+  }, 350); // this is how much time debounce will wait to fire the query
+
+  render() {
+    resetIdCounter();
+    return (
+      <SearchStyles>
+        <Downshift
+          onChange={routeToItem}
+          itemToString={item => (item === null ? '' : item.title)}
+        >
+          {({
+            getInputProps,
+            getItemProps,
+            isOpen,
+            inputValue,
+            highlightedIndex
+          }) => (
+            <div>
+              <ApolloConsumer>
+                {client => (
+                  <input
+                    {...getInputProps({
+                      type: 'search',
+                      placeholder: 'Search for an item',
+                      id: 'search',
+                      className: this.state.laoading ? 'loading' : '',
+                      onChange: e => {
+                        e.persist();
+                        this.onChange(e, client);
+                      }
+                    })}
+                  />
+                )}
+              </ApolloConsumer>
+              {isOpen && (
+                <DropDown>
+                  {this.state.items.map((item, index) => (
+                    <DropDownItem
+                      {...getItemProps({ item })}
+                      key={item.id}
+                      highlighted={index === highlightedIndex}
+                    >
+                      <img width='50' src={item.image} alt={item.title} />
+                      {item.title}
+                    </DropDownItem>
+                  ))}
+                  {!this.state.items.length && !this.state.loading && (
+                    <DropDownItem> Nothing Found</DropDownItem>
+                  )}
+                </DropDown>
+              )}
+            </div>
+          )}
+        </Downshift>
+      </SearchStyles>
+    );
+  }
+}
+
+export default AutoComplete;
